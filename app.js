ba6ea06a04fbb39421e3a6e6c8d9d904425f1d53
“Jelido” <21gayo2@gmail.com>
2023-11-07 20:31:15 -0300

    feature app

--- app.js
+++ app.js
@@ -1,33 +1,9 @@
-// let titulo = document.querySelector('h1')
-// titulo.innerHTML = 'Jogo do número secreto'
+let elementoInput1 = document.getElementById("input1")
+let elementoInput2 = document.getElementById("input2")
+let soma
 
-// let paragrafo = document.querySelector('p')
-// paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
-
-
-let NumeroSecreto = gerarNumeroAleatorio()
-
-function exibirTextoNaTela(tag,texto){
-
-    let campo = document.querySelector(tag)
-    campo.innerHTML = texto
-    
+function SomaNumeros(){
+   
+soma = Number(elementoInput1.value) + Number(elementoInput2.value)
+alert(soma)
 }
-
-exibirTextoNaTela('h1', 'Soma')
-exibirTextoNaTela('p', 'Coloque os números para a soma')
-
-
-
-function VerificarChute(){
-
-    alert()
-
-}
-
-function gerarNumeroAleatorio(){
-
-    return Math.floor(Math.random() * (10 - 1) + 1)
-
-}
-
